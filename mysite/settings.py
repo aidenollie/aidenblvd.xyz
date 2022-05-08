5582ffeffbcc25ebe10f071cd44fb0f926112ff2
Aiden Boulevard <aidenollie9@gmail.com>
2022-05-08 17:36:49 -0500

    changed proc and wsgi

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(DJANGO_SECRET) #'django-insecure-$i6p@iq41xouti(64i%787s7hm)2e02lxltp&*(7ssrvp0pi32'
+SECRET_KEY = os.environ.get('DJANGO_SECRET') #'django-insecure-$i6p@iq41xouti(64i%787s7hm)2e02lxltp&*(7ssrvp0pi32'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
