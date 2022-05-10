d1b0a9a239eefc02bd26b7d894fe1e3f8a3a5dc4
aidenollie <aidenollie9@gmail.com>
2022-05-10 10:35:19 -0500

    fixed images on local server

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 urlpatterns = [
@@ -7,3 +11,5 @@ path("", views.home, name="home"),
 path("home/", views.home, name="home"),
 path("contact", views.contact, name="contact"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
