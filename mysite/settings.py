9a166338e74556518f252b53a8c7779967dbf2c8
aidenollie <aidenollie9@gmail.com>
2022-05-10 23:41:16 -0500

    Debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')  == 'True'
+DEBUG = True#os.environ.get('DEBUG')  == 'True'
 
 
 ALLOWED_HOSTS = ["https://aidendjangoapp.herokuapp.com/",
